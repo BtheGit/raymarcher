9223b54947a3f4a7016c5688cbe2f103c84748a3
BtheGit <brendanwebdev@gmail.com>
2019-05-15 10:58:53 -0400

    Add vector methods

--- classes/Vector.js
+++ classes/Vector.js
@@ -3,4 +3,24 @@ class Vector {
     this.x = x;
     this.y = y;
   }
+
+  add(vector){
+    return new Vector(this.x + vector.x, this.y + vector.y);
+  }
+
+  scale(scalar){
+    return new Vector(this.x * scalar, this.y * scalar);
+  }
+
+  cross(vector){
+
+  }
+
+  dot(vector){
+
+  }
+
+  map(callback){
+    return new Vector(callback(this.x), callback(this.y));
+  }
 }
