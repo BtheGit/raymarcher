cc03441bf65544c96e1a097faee29fd11c06cef8
Brendan Beltz <brendanwebdev@gmail.com>
2023-06-27 13:00:13 -0400

    Replace webpack with vite

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/index.js"),
+      name: "Raymarcher",
+      // the proper extensions will be added
+      fileName: "raymarcher",
+    },
+    sourcemap: true,
+  },
+});
